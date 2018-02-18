d7a1e5667c9a822bfd0d35a459f96554c689347e
jomesan <mellado.proyecto2017@gmail.com>
2018-02-18 21:30:53 +0100

    funcionando routelink y visualizando graficas

--- src/main/webapp/app/graficas/graficas.component.ts
+++ src/main/webapp/app/graficas/graficas.component.ts
@@ -1,24 +1,21 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import {JhiAlertService, JhiEventManager} from 'ng-jhipster';
-import {Linea, LineaService} from '../entities/linea';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute } from '@angular/router';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
-import { Account, LoginModalService, Principal } from '../shared';
-import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
-import {Subscription} from 'rxjs/Subscription';
+import { Principal} from '../shared';
 
 @Component({
     selector: 'jhi-graficas',
-    templateUrl: './graficas.component.html',
-    styleUrls: [
-        'graficas.css'
-    ]
+    templateUrl: './graficas.component.html'
 })
 export class GraficasComponent implements OnInit, OnDestroy {
-    account: Account;
-    modalRef: NgbModalRef;
-    private lineas: Linea [];
+
+    currentAccount: any;
     eventSubscriber: Subscription;
+    private subscription: Subscription;
+    linea: number;
+
     public lineChartData: Array<any> = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
@@ -56,30 +53,41 @@ export class GraficasComponent implements OnInit, OnDestroy {
     ];
     public lineChartLegend = true;
     public lineChartType = 'line';
+
     constructor(
-        private lineaService: LineaService,
-        private principal: Principal,
         private jhiAlertService: JhiAlertService,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private principal: Principal,
+        private route: ActivatedRoute
     ) {
     }
 
-    loadAll() {
-        this.lineaService.query().subscribe(
-            (res: HttpResponse<Linea[]>) => {
-                this.lineas = res.body;
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
-    }
     ngOnInit() {
         this.principal.identity().then((account) => {
-            this.account = account;
+            this.currentAccount = account;
         });
-        this.registerAuthenticationSuccess();
-        this.loadAll();
-        this.registerChangeInLineas();
+        this.subscription = this.route.params.subscribe((params) => {
+            this.linea = (params['id']); });
+        this.registerChangeInGraficas();
+    }
+
+    registerChangeInGraficas() {
+        this.eventSubscriber = this.eventManager.subscribe(
+            'GraficasListModification',
+            (response) => this.subscription.unsubscribe()
+        );
+    }
+    previousState() {
+        window.history.back();
+    }
+
+    ngOnDestroy() {
+            this.subscription.unsubscribe();
+            this.eventManager.destroy(this.eventSubscriber);
+    }
+
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 
     public randomize(): void {
@@ -101,32 +109,4 @@ export class GraficasComponent implements OnInit, OnDestroy {
     public chartHovered(e: any): void {
         console.log(e);
     }
-
-    registerChangeInLineas() {
-        this.eventSubscriber = this.eventManager.subscribe('lineaListModification', (response) => this.loadAll());
-    }
-
-    private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
-    }
-
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', (message) => {
-            this.principal.identity().then((account) => {
-                this.account = account;
-            });
-        });
-    }
-
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
-    }
-
-    login() {
-        this.modalRef = this.loginModalService.open();
-    }
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
-    }
-
 }
